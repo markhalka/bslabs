1ce69b13062888180f75c715c3bf0744a5840f3d
markhalka <mahalka@uwaterloo.ca>
2023-11-01 13:48:56 -0400

    about to start designing v3

--- software/Arm/ArmDemo/ArmDemo/ArmDemo.ino
+++ software/Arm/ArmDemo/ArmDemo/ArmDemo.ino
@@ -1,29 +1,22 @@
 #include <Solver.h>
 #include <UserInterface.h>
 #include <ControlMessage.h>
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
-SoftwareSerial software_serial{ 7, 8 };
-Solver solver{&software_serial};
-UserInterface user_interface{&software_serial};
+
+Solver solver{&Serial1};
+UserInterface user_interface{&Serial1};
 
 void setup() {
-  software_serial.begin(9600);
-  software_serial.println("starting demo...");
+  Serial1.begin(9600);
+  Serial1.println("starting demo...");
   user_interface.begin();
   solver.begin();
 }
 
-void readCommand(const Command& command){
-  software_serial.println(command.type == SERVO_COMMAND);
-  for(int i = 0; i < N_JOINTS;  i++){
-    software_serial.println(command.data.servo_command.servo_delta[i]);
-  }
-}
 void loop() {
   if(user_interface.hasMessage()){
       Command next_message = user_interface.getNextMessage();
       solver.execute(next_message);
-      // readCommand(next_message);
   }
 }
